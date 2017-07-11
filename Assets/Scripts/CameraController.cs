1d181a97b0433c96a88876fb58e3cd01449bd713
lucasanjosmoraes <lucas.anjos.moraes@gmail.com>
2017-07-11 14:58:37 -0400

    add arquivos do projeto

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+
+    public GameObject player;
+
+    //Eh private pq eh um valor que precisa ser determinado aqui no codigo, apenas
+    private Vector3 offset;
+
+    void Start ()
+    {
+    	//Pega a distancia entre a posicao fixa da camera e a atual do jogador
+        offset = transform.position - player.transform.position;
+    }
+    
+    //Para acompanhamento das cameras, animaçao procedural e capturar os status mais recentes
+    //LateUpdate eh melhor do que Update apenas
+    void LateUpdate ()
+    {
+    	//adapta a variavel da camera a cada movimento do jogador
+        transform.position = player.transform.position + offset;
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+    public float speed;
+    public Text countText;
+    public Text winText;
+
+    private Rigidbody rb;
+    private int count;
+
+    void Start ()
+    {
+        rb = GetComponent<Rigidbody>();
+        count = 0;
+        SetCountText ();
+        winText.text = "";
+    }
+
+    void FixedUpdate ()
+    {
+    	// //pega os comandos do teclado
+     //    float moveHorizontal = Input.GetAxis ("Horizontal");
+     //    float moveVertical = Input.GetAxis ("Vertical");
+
+     //    //Vector3 se refere aos 3 eixos canonicos
+     //    Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+
+        Vector3 dir = Vector3.zero;
+        dir.x= Input.acceleration.x;
+        dir.z= Input.acceleration.y;
+        if(dir.sqrMagnitude>1)
+            dir.Normalize();
+
+        // dir *=Time.deltaTime;
+        // transform.Translate(dir * speed);
+
+        rb.AddForce (dir * speed);
+    }
+
+    void OnTriggerEnter(Collider other) 
+    {
+        if (other.gameObject.CompareTag ("Pick Up"))
+        {
+            other.gameObject.SetActive (false);
+            count = count + 1;
+            SetCountText ();
+        }
+    }
+
+    void SetCountText ()
+    {
+        countText.text = "Count: " + count.ToString ();
+        if (count >= 12)
+        {
+            winText.text = "You Win!";
+        }
+    }
+}
